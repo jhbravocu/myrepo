88b9a0a529403db8b24e58ba13fdae65d46dbe6d
odoo <odoo@gmail.com>
2024-01-30 18:48:58 -0300

    Actualizando dev_joel_17

--- cargar_uyu/__manifest__.py
+++ cargar_uyu/__manifest__.py
@@ -18,11 +18,11 @@ Long description of module's purpose
     'version': '17.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['grp_catalogos'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
